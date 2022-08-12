99156eb3abf4e8332a4c9d12fa1958443afcb7e5
AleMar21430 <mar21430@uvg.edu.gt>
2022-08-11 20:27:36 -0600

    update

--- Lab2/app/src/main/java/com/marti21430/lab2/MainActivity.kt
+++ Lab2/app/src/main/java/com/marti21430/lab2/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.marti21430.lab2
 
+import android.widget.Button
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +9,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toast_mesg = findViewById(R.id.button2) as Button
+        toast_mesg.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Alejandro Jose Martinez de Leon 21430", Toast.LENGTH_SHORT).show()
+        }
     }
 }
