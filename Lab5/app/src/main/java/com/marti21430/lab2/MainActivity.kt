37651657490c054a1b566aed56d8403039a58dda
AleMar21430 <97700487+Pekoyo@users.noreply.github.com>
2022-08-16 18:09:11 -0600

    Update MainActivity.kt

--- Lab5/app/src/main/java/com/marti21430/lab2/MainActivity.kt
+++ Lab5/app/src/main/java/com/marti21430/lab2/MainActivity.kt
@@ -15,18 +15,23 @@ class MainActivity : AppCompatActivity() {
         toast_mesg.setOnClickListener {
             Toast.makeText(this@MainActivity, "Alejandro Jose Martinez de Leon 21430", Toast.LENGTH_SHORT).show()
         }
-        val Descargar = findViewById(R.id.button) as Button
-        Descargar.setOnClickListener{
+        val Direcciones = findViewById(R.id.button) as Button
+        Direcciones.setOnClickListener{
             val intent = Intent(Intent.ACTION_VIEW)
             intent.setData(Uri.parse("geo:14.611189270542207,-90.485456403024"))
             val Maps = Intent.createChooser(intent, "Launch Maps")
             startActivity(Maps)
         }
-            val Detalles = findViewById(R.id.button4) as Button
+        val Detalles = findViewById(R.id.button4) as Button
         Detalles.setOnClickListener{
             val intent = Intent(Intent.ACTION_VIEW)
             val Maps = Intent.createChooser(intent, "Launch Maps")
             startActivity(Maps)
         }
+        val Descargar = findViewById(R.id.button5) as Button
+        Descargar.setOnClickListener{
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=com.YoStarEN.Arknights&hl=es_GT&gl=US")))
+        }
+
     }
 }
