30dc6fa490700b929247106ba243fdf99c9be378
AleMar21430 <97700487+Pekoyo@users.noreply.github.com>
2022-08-21 17:31:23 -0600

    Finished

--- Lab6/app/src/main/java/com/marti21430/lab6/ThirdFragment.kt
+++ Lab6/app/src/main/java/com/marti21430/lab6/ThirdFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.marti21430.lab6.databinding.FragmentThirdBinding
 
+
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
@@ -28,7 +29,7 @@ class ThirdFragmentFragment : Fragment() {
         return binding.root
 
     }
-
+var Counter = 0
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -37,6 +38,10 @@ class ThirdFragmentFragment : Fragment() {
         }
         binding.buttonSearch.setOnClickListener {
             findNavController().navigate(R.id.action_ThirdFragment_to_SecondFragment)
+        }
+        binding.buttonFavorite.setOnClickListener {
+            Counter = Counter+1
+            binding.buttonCount.setText(Counter.toString())
         }
     }
 
