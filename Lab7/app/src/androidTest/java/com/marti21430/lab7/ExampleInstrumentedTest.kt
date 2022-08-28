78b47429492cc6a7b528ae7027e012ffa315183e
AleMar21430 <97700487+Pekoyo@users.noreply.github.com>
2022-08-28 10:35:08 -0600

    Finally Works ;_;

--- Lab7/app/src/androidTest/java/com/marti21430/lab7/ExampleInstrumentedTest.kt
+++ Lab7/app/src/androidTest/java/com/marti21430/lab7/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.marti21430.lab7", appContext.packageName)
+        assertEquals("com.marti21430.lab6", appContext.packageName)
     }
 }
