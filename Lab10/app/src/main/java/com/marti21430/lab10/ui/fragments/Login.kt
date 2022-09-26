48d0af71ac79b0232c83ec51cbbb9d5ee02d216f
AleMar21430 <97700487+Pekoyo@users.noreply.github.com>
2022-09-25 20:15:34 -0600

    splash screen

--- Lab10/app/src/main/java/com/marti21430/lab10/ui/fragments/Login.kt
+++ Lab10/app/src/main/java/com/marti21430/lab10/ui/fragments/Login.kt
@@ -7,8 +7,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.navigation.fragment.findNavController
+import coil.load
+import coil.request.CachePolicy
+import coil.transform.CircleCropTransformation
 import com.marti21430.lab10.R
 import com.marti21430.lab10.databinding.FragmentLoginBinding
 import com.marti21430.lab10.ui.activities.MyApplication.Companion.username
@@ -17,14 +21,14 @@ import com.marti21430.lab10.ui.adapters.CharacterAdapter
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class Login : Fragment() {
+class Login : Fragment(R.layout.fragment_login) {
 
     private var _binding: FragmentLoginBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
-
+    private lateinit var Splash_Image: ImageView
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -38,10 +42,19 @@ class Login : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val prefs = this.requireActivity().getSharedPreferences("MyUserPrefs", Context.MODE_PRIVATE)
-
+        view.apply {
+            Splash_Image = findViewById(R.id.Splash_Image)
+        }
         if (prefs.getBoolean("Logged", true)){
             findNavController().navigate(R.id.action_login_to_characterListFragment)
         }
+
+        Splash_Image.load("https://occ.a.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABekGfcjcxKQH35sJxlr2te1JNYCwXqpovwMvTf4gzZ1ihzzUY0M8YcEijgvL_ClD5aIMWEY-hKIZVmgthEVssV7SthGRm2xVXkuwzpphv6u0.png?r=a66") {
+            placeholder(R.drawable.ic_downloading)
+            error(R.drawable.ic_error)
+            memoryCachePolicy(CachePolicy.ENABLED)
+        }
+
         binding.ButtonLoginToWelcome.setOnClickListener {
 
             val inputusername = getView()?.findViewById(R.id.Text_Input_Mail) as EditText
