6c8b44106a07a109e3ebbce876ae01c854c4d99c
AleMar21430 <97700487+Pekoyo@users.noreply.github.com>
2022-09-26 06:59:53 -0600

    Password

--- Lab10/app/src/main/java/com/marti21430/lab10/ui/activities/MyApplication.kt
+++ Lab10/app/src/main/java/com/marti21430/lab10/ui/activities/MyApplication.kt
@@ -4,8 +4,7 @@ import android.app.Application
 
 class MyApplication : Application() {
     companion object {
-        var username: String = ""
-        //"jcdurini@uvg.edu.gt"
+        var username: String = "jcdurini@uvg.edu.gt"
     }
     override fun onCreate() {
         super.onCreate()
